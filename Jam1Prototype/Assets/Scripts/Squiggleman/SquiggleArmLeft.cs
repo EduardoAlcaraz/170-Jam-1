20313bcd7a6f52f0d75c299d4d36eb6c0c808229
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2022-10-13 17:18:50 -0700

    limb rotation detection
    Now detects when the limbs are rotated into the correct positions.

--- Jam1Prototype/Assets/Scripts/Squiggleman/SquiggleArmLeft.cs
+++ Jam1Prototype/Assets/Scripts/Squiggleman/SquiggleArmLeft.cs
@@ -11,6 +11,7 @@ public class SquiggleArmLeft : MonoBehaviour
     public GameObject rightLeg;
     private float increment = 30f;
     private bool ending = false;
+    System.Random rand = new System.Random();
 
     // Start is called before the first frame update
     void Start()
@@ -20,29 +21,44 @@ public class SquiggleArmLeft : MonoBehaviour
         leftLeg = GameObject.Find("left_leg2");
         rightLeg = GameObject.Find("right_leg2");
 
-        leftArm.transform.Rotate(0f, 0f, increment*11, Space.Self);
-        rightArm.transform.Rotate(0f, 0f, increment*9, Space.Self);
-        leftLeg.transform.Rotate(0f, 0f, increment*7, Space.Self);
-        rightLeg.transform.Rotate(0f, 0f, increment*4, Space.Self);
+        leftArm.transform.Rotate(0f, 0f, increment*11, Space.Self);  //330
+        rightArm.transform.Rotate(0f, 0f, increment*9, Space.Self);  //270
+        leftLeg.transform.Rotate(0f, 0f, increment*7, Space.Self);  //210
+        rightLeg.transform.Rotate(0f, 0f, increment*4, Space.Self);  //120
     }
 
     // Update is called once per frame
     void Update()
     {
+        float newAngleLA;
+        float newAngleRA;
+        float newAngleLL;
+        float newAngleRL;
+        
+
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
             leftArm.transform.Rotate(0f, 0f, increment, Space.Self);
+            newAngleLA = leftArm.transform.localEulerAngles.z;
+            if (newAngleLA > -1 && newAngleLA < 1) Debug.Log("Left Arm zero"); 
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
             rightArm.transform.Rotate(0f, 0f, -increment, Space.Self);
+            newAngleRA = rightArm.transform.localEulerAngles.z;
+            if (newAngleRA > -1 && newAngleRA < 1) Debug.Log("Right Arm Zero");
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
             leftLeg.transform.Rotate(0f, 0f, increment, Space.Self);
+            newAngleLL = leftLeg.transform.localEulerAngles.z;
+            //Debug.Log(leftLeg.transform.localEulerAngles.z.ToString() + "    " + rand.Next(0, 100000).ToString());
+            if (newAngleLL > -1 && newAngleLL < 1) Debug.Log("Left Leg zero");
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
             rightLeg.transform.Rotate(0f, 0f, -increment, Space.Self);
+            newAngleRL = rightLeg.transform.localEulerAngles.z;
+            if (newAngleRL > -1 && newAngleRL < 1) Debug.Log("Right Leg zero");
         }
         
     }
