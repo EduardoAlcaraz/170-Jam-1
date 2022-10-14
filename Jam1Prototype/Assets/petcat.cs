eba7028a43388ae44fe9025bce717cc4b0a4ac36
Malachi Maldonado <m.maldonado1792@gmail.com>
2022-10-13 17:02:09 -0700

    implemented cat petting!

--- Jam1Prototype/Assets/petcat.cs
+++ Jam1Prototype/Assets/petcat.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class petcat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Sprite cat_down;
+    public Sprite cat_up;
+    SpriteRenderer s;
+    void Start(){
+        s = gameObject.GetComponent<SpriteRenderer>();
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool isPressed = Input.GetKeyDown(KeyCode.Space);
+        if(isPressed){
+            if(s.sprite == cat_down){
+                s.sprite = cat_up;
+            }else{
+                s.sprite = cat_down;
+            }
+        }
     }
 }
